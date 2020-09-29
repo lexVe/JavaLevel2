8d2bec9816d5216af03a14ddaeb3682c35595dac
lexVe <cnah4@rambler.ru>
2020-09-29 09:17:09 +0300

    Correction
    Мне кажется некоторые методы вообще не учавствуют в программе.

--- Lesson1.Bubbles/src/Lesson1/BackGround.java
+++ Lesson1.Bubbles/src/Lesson1/BackGround.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class BackGround{
 
-    private static int red = 0, green = 0, blue = 0;
+    private static int red = ranNum(), green = ranNum(), blue = ranNum();
 
     private static int ranNum(){
         int num = (int)Math.random()*255;
@@ -18,13 +18,13 @@ public class BackGround{
         private static Color bgColor(){
         red++;
         green++;
-        blue=blue+20;
+        blue=blue+2;
         if(red >= 255){red = ranNum();}
         else if(green >= 255){green = ranNum();}
         else if(blue >= 255){blue = ranNum();}
 
         if(red==255 || green ==255 || blue == 255){
-            return new Color(red=0, green=0, blue=0);
+            return new Color(red=ranNum(), green=ranNum(), blue=ranNum());
         }
             return new Color(red, green, blue);
 
